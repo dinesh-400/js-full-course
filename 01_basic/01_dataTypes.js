dda194a06370bcbaee79c59f5d62939edbb7d87b
Dinesh Sahoo <71336294+dinesh-400@users.noreply.github.com>
2024-06-02 16:56:49 +0000

    Data Types

---
+++ 01_basic/01_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+let name = "Dinesh"
+let age = 23
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint
+//string => "
+//boolean
+//null => standalone value
+//undefined => 
+//symbol
+
+
+//object
+
+console.log(typeof undefined);
+console.log(typeof null);
