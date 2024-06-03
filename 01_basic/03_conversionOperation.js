93603f8d8c8cb80ea3d017e01ebeb42a805fcd21
Dinesh Sahoo <71336294+dinesh-400@users.noreply.github.com>
2024-06-03 16:59:02 +0000

    Data Type Conversion & Operations

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -22,9 +22,39 @@
 // "" => false
 // "Dinesh" => true
 
-let num = 33
+// let num = 33
 
-let stringNum = String(num)
+// let stringNum = String(num)
 
-console.log(stringNum);
-console.log(typeof(stringNum));
+// console.log(stringNum);
+// console.log(typeof(stringNum));
+
+// *****************  Operations ********************
+
+// let val = 3
+// let negVal = -val
+
+// console.log(negVal);
+
+// let str1 = "Hello"
+// let str2 = " Dinesh"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(3 + 4 * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100;
+// ++gameCounter;
+console.log(++gameCounter);
