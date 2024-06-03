3d037233f89990a3b41f2c798e3d064d950e7a7c
Dinesh Sahoo <71336294+dinesh-400@users.noreply.github.com>
2024-06-03 16:40:56 +0000

    Data Types Conversion

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,30 @@
+// let score = "Dinesh";
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = "Dinesh"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+// console.log(typeof(booleanIsLoggedIn));
+
+// 1 => true; 0 => false
+// "" => false
+// "Dinesh" => true
+
+let num = 33
+
+let stringNum = String(num)
+
+console.log(stringNum);
+console.log(typeof(stringNum));
