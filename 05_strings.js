8b3a118ed80dc16ce0c61e2d392bdf083b332302
Dinesh Sahoo <71336294+dinesh-400@users.noreply.github.com>
2024-07-01 15:30:33 +0000

    String With Some Functions

---
+++ 05_strings.js
@@ -0,0 +1,34 @@
+const name = "Dinesh"
+const repoCount = 10
+
+// console.log(name + repoCount);
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('dinesh-dc-com')
+
+// console.log(gameName.__proto__);
+
+// console.log(gameName.charAt(5));
+
+// console.log(gameName.indexOf('s'));
+
+// const newString = gameName.substring(0, 4)
+
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 4)
+
+// console.log(anotherString);
+
+// const newString = "     Dinesh    "
+// console.log(newString);
+// console.log(newString.trim());
+
+// const url = "https://dinesh.com/dinesh%20sahoo"
+
+// console.log(url.replace('%20', '-'))
+
+// console.log(url.includes('dinesh'));
+
+console.log(gameName.split('-'));
